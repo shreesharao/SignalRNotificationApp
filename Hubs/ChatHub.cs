31138aaea29ab5441d7973aaac294b7d271bfb67
shreesharao <sshreesha4@gmail.com>
2020-10-22 12:44:19 +0530

    Initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Clients.All.SendAsync("broadcastMessage", $"'Patient {i}' results are ready to view");
+                await Task.Delay(300);
+            }
+            // Call the broadcastMessage method to update clients.
+        }
+    }
+}
